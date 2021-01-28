cb8ad3ab1fbcfa9e5eb450d86bb2e74e978f9196
F-Gabriel-Braga <braagaa.infor2@gmail.com>
2021-01-27 22:13:39 -0300

    Commit 01 | Agenda Telefônica | Construção do Projeto

---
+++ controller/MenuPrincipalController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MenuPrincipalController {
+
+    @FXML
+    private Button btn_pesquisar;
+
+    @FXML
+    private Button btn_ad_usuario;
+
+    @FXML
+    private Button btn_sair;
+
+    @FXML
+    private AnchorPane conteiner;
+
+}
