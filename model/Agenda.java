762a962b86700e37300be63cc0a1e9e0399ac636
F-Gabriel-Braga <braagaa.infor2@gmail.com>
2021-01-29 13:06:07 -0300

    Commit 06 | Agenda Telefônica | Implementação da Conexão com Banco de Dados

--- model/Agenda.java
+++ model/Agenda.java
@@ -3,52 +3,62 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Agenda {
 
-    private Connection connection = null;
-
-    private String hostName = null;
-    private String userName = null;
-    private String password = null;
-    private String url = null;
-    private String dataBaseName = null;
-    private String dataBasePrefix = null;
-    private String dabaBasePort;
-
     public Agenda() {
-        //"jdbc: mysql:/localhost:3306/meu_bd";
-        hostName = "127.0.0.1";
-        userName = "root";
-        password = "";
-        dataBaseName = "agenda";
-        dataBasePrefix = "jdbc:mysql:/";
-        dabaBasePort = "3306";
-        url = dataBasePrefix + hostName + ":"+dabaBasePort+"/" + dataBaseName + "/";
+        criarBancoDeDados(bancoDeDados);
     }
 
-    public Connection openConnection() {
+    String url = "jdbc:mysql://127.0.0.1:3306?verifyServerCertificate=false&useSSl=true";
+    final private String usuario = "root";
+    final private String senha = "";
+
+    final private String bancoDeDados = "agenda";
+    final private String tabela = "contato";
+
+    Connection conexao = null;
+
+    private void abrirConexao() {
         try {
-            if(connection == null) {
-                connection = DriverManager.getConnection(url, userName, password);
-            }
-            else if (connection.isClosed()) {
-                System.err.println("ERRO: Conexão nula.");
-            }
+            conexao = DriverManager.getConnection(url, usuario, senha);
         }
-        catch (SQLException e) {
-            System.err.println("ERRO: " + e.getCause());
+        catch(SQLException e) {
+            System.err.println("ERRO (Abrir Conexao): " + e.getMessage());
         }
-        return connection;
     }
 
-    public void closeConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                System.err.println("ERRO: " + e.getCause());
-            }
+    private void fecharConexao() {
+        try {
+            conexao.close();
         }
+        catch(SQLException e) {
+            System.err.println("ERRO (Fechar Conexao): " + e.getMessage());
+        }
+    }
+
+    private void criarBancoDeDados(String bancoDeDados) {
+        abrirConexao();
+        try {
+            Statement declaracao = conexao.createStatement();
+            String sql = String.format("CREATE DATABASE IF NOT EXISTS %s;", bancoDeDados);
+            declaracao.executeUpdate(sql);
+        } catch (SQLException e) {
+            System.err.println("ERRO (Criar Banco De Dados): " + e.getCause());
+        }
+        fecharConexao();
+    }
+
+    public void adicionarContato(Contato contato) {
+        abrirConexao();
+        try {
+            Statement declaracao = conexao.createStatement();
+            String sql = String.format("INSERT INTO contato (nome, numeroTelefone, endereco, email) VALUES ('%s', '%s', '%s', '%s');", contato.getNome(), contato.getNumeroTelefone(), contato.getEndereco(), contato.getEmail());
+            declaracao.executeUpdate(sql);
+        } catch (SQLException e) {
+            System.err.println("ERRO (Adicionar Contato): " + e.getCause());
+        }
+        fecharConexao();
     }
 }
